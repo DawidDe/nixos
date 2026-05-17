499fedf62e10461cc31a7daa16a04fbf1da1e7a4
Dawid Imbrzykowski <dawidimb@proton.me>
2026-05-17 13:31:01 +0200

    Refactor overlays for aarch64 support in podman.nix

--- pi/services/podman.nix
+++ pi/services/podman.nix
@@ -6,9 +6,12 @@
   
   # On aarch64, libkrunfw doesn't support variants, so we disable libkrun entirely
   # This is fine for Raspberry Pi since basic container functionality doesn't need it
-  nixpkgs.overlays = lib.optionals pkgs.stdenv.isAarch64 [
-    (final: prev: {
-      libkrun = null;
-    })
+  nixpkgs.overlays = [
+    (final: prev:
+      if prev.stdenv.isAarch64 then
+        { libkrun = null; }
+      else
+        { }
+    )
   ];
 }
