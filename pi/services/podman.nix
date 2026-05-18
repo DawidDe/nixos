0ba78933986e7fe3e649a56eeb21b226b8c9d7ff
Dawid Imbrzykowski <dawidimb@proton.me>
2026-05-18 18:57:57 +0200

    removed junk

--- pi/services/podman.nix
+++ pi/services/podman.nix
@@ -3,15 +3,4 @@
 {
   # Enable Podman
   virtualisation.podman.enable = true;
-  
-  # On aarch64, libkrunfw doesn't support variants, so we disable libkrun entirely
-  # This is fine for Raspberry Pi since basic container functionality doesn't need it
-  nixpkgs.overlays = [
-    (final: prev:
-      if prev.stdenv.isAarch64 then
-        { libkrun = null; }
-      else
-        { }
-    )
-  ];
 }
