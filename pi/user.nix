9d4c4695310c7d6a5c58abd89d3c06311bad0274
Dawid Imbrzykowski <dawidimb@proton.me>
2026-05-18 12:46:06 +0200

    added basic password for sd image

--- pi/user.nix
+++ pi/user.nix
@@ -8,6 +8,7 @@
     openssh.authorizedKeys.keys = [
       "sk-ecdsa-sha2-nistp256@openssh.com AAAAInNrLWVjZHNhLXNoYTItbmlzdHAyNTZAb3BlbnNzaC5jb20AAAAIbmlzdHAyNTYAAABBBKzkzCs5JnFDhBB7YAmnzcd0S6tbMsV1XGJ3B4aod+TdJUI0ngLLyFQxJVpXivExqqHUtPH14HdpV5qWVx5NnV4AAAALdGVybWl1cy5jb20="
     ];
+    hashedPassword = "$y$j9T$9P0tRLmzfTI2GfS8u8k2F.$5sG0QnX8V0ukUVUmbrL6/cqPREm2cfRmoy2otFEy7q9";
   };
 
   users.groups = {
