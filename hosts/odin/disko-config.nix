8293cae564a832d372628fefeb4f526a214c29a3
Dawid Imbrzykowski <79714759+DawidDe@users.noreply.github.com>
2026-09-17 15:39:20 +0200

    fixed odin disko-config

--- hosts/odin/disko-config.nix
+++ hosts/odin/disko-config.nix
@@ -25,17 +25,16 @@
                 mountpoint = "/";
               };
             };
+            home = {
+              number = 3;
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/home";
+                mountOptions = [ "defaults" "noatime" ];
+              };
+            };
           };
-        };
-      };
-      home = {
-        type = "disk";
-        device = "/dev/disk/by-id/nvme-KINGSTON_SNV2S2000G_50026B7382B118EA-part3";
-        content = {
-          type = "filesystem";
-          format = "ext4";
-          mountpoint = "/home";
-          mountOptions = [ "defaults" "noatime" ];
         };
       };
     };
