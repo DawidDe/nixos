73d3df231c1fcedea7a1c100c59628e75e926bce
Dawid Imbrzykowski <79714759+DawidDe@users.noreply.github.com>
2026-09-17 15:03:54 +0200

    fixed disko-config for odin

--- hosts/odin/disko-config.nix
+++ hosts/odin/disko-config.nix
@@ -9,7 +9,7 @@
           partitions = {
             ESP = {
               type = "EF00";
-              size = "1G";                    # exactly matches nvme0n1p1
+              size = "1G";
               content = {
                 type = "filesystem";
                 format = "vfat";
@@ -17,23 +17,8 @@
                 mountOptions = [ "umask=0077" ];
               };
             };
-            swap = {
-              size = "8G";                    # exactly matches nvme0n1p2
-              content = {
-                type = "swap";
-              };
-            };
-            home = {
-              size = "1638G";                 # ≈ current nvme0n1p3 (slightly smaller)
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/home";
-                # Disko will *not* format it because a filesystem already exists
-              };
-            };
             root = {
-              size = "100%";                  # takes the rest (≈ current nvme0n1p4)
+              size = "200G";
               content = {
                 type = "filesystem";
                 format = "ext4";
@@ -41,6 +26,16 @@
               };
             };
           };
+        };
+      };
+      home = {
+        type = "disk";
+        device = "/dev/disk/by-id/nvme-KINGSTON_SNV2S2000G_50026B7382B118EA-part3";
+        content = {
+          type = "filesystem";
+          format = "ext4";
+          mountpoint = "/home";
+          mountOptions = [ "defaults" "noatime" ];
         };
       };
     };
