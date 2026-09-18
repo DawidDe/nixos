dff5665d5435a09f5cdec2ae411bcb7b71ae314f
Dawid Imbrzykowski <79714759+DawidDe@users.noreply.github.com>
2026-09-18 18:54:18 +0200

    add a temporary mount

--- modules/services/smb.nix
+++ modules/services/smb.nix
@@ -5,5 +5,15 @@
     enable = true;
     
     smbd.enable = true;
+    
+    settings = {
+      mount = {
+        path = "/home/dawid/mount";
+        "read only" = "no";
+        "guest ok" = "yes";
+        "force user" = "root";
+        "force group" = "root";
+      };
+    }; 
   };
 }
