6cfe83ebd5e3b9dc6ad295aaff95fa987dc61e95
Dawid J. Imbrzykowski <dawidimb@proton.me>
2026-09-23 17:52:44 +0200

    add yubikey authenticator. removing antigravity

--- hosts/odin/packages.nix
+++ hosts/odin/packages.nix
@@ -30,7 +30,7 @@
     vscodium
     superfile
     drawio
-    antigravity
+    yubioath-flutter
     basalt.packages.x86_64-linux.basalt-launcher-dev
 
     # ── Development / Programming ────────────────────────────
