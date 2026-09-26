f8f3ea738189dec80854f62e5d5b99e42dfcb68b
Dawid J. Imbrzykowski <dawidimb@proton.me>
2026-09-26 14:44:48 +0200

    fixed openbao

--- modules/services/openbao.nix
+++ modules/services/openbao.nix
@@ -1,14 +1,35 @@
 { config, lib, pkgs, ...}:
 
 {
+  users = {
+    users.openbao = {
+      isSystemUser = true;
+      group = "openbao";
+      uid = 1100;
+    };
+    
+    groups.openbao = {
+      gid = 1100;
+    };
+  };
+
+  systemd.tmpfiles.rules = [
+    "d /opt/openbao 0700 openbao openbao"
+  ];
+
   services.openbao = {
     enable = true;
 
     settings = {
       ui = true;
 
+      storage.raft = {
+        path = "/opt/openbao"
+      };
+
       listener.default = {
         type = "tcp";
+        address = "127.0.0.1:8200"
       };
     };
   };
