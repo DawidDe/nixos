855f5aad78557f1831c2053e2e9002733dbc50a5
Dawid J. Imbrzykowski <dawidimb@proton.me>
2026-09-26 15:18:35 +0200

    added firewall rules for pangolin

--- modules/services/pangolin.nix
+++ modules/services/pangolin.nix
@@ -1,6 +1,13 @@
 { config, lib, pkgs, ...}:
 
 {
+  networking.firewall.extraInputRules = ''
+    iifname { "end0" } ip saddr 192.168.178.1 tcp dport 80 accept
+    iifname { "end0" } ip saddr 192.168.178.1 tcp dport 443 accept
+    iifname { "end0" } ip saddr 192.168.178.1 udp dport 21820 accept
+    iifname { "end0" } ip saddr 192.168.178.1 udp dport 51820 accept
+  '';
+
   sops.secrets."pangolin-env" = {
     sopsFile = ../../secrets/pangolin.env;
     format = "dotenv";
