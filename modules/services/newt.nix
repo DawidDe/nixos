84e0db95a0c772beea16503c4d6f3e467a30b267
Dawid J. Imbrzykowski <dawidimb@proton.me>
2026-09-26 15:42:38 +0200

    added newt. removed unneccassary keys in pangolin.env

---
+++ modules/services/newt.nix
@@ -0,0 +1,18 @@
+{ config, lib, pkgs, ...}:
+
+{
+  sops.secrets."newt-env" = {
+    sopsFile = ../../secrets/newt.env;
+    format = "dotenv";
+  };
+
+  services.newt = {
+    enable = true;
+
+    settings = {
+      endpoint = "https://pangolin.dawidde.de"
+    };
+
+    environmentFile = config.sops.secrets.newt-env.path;
+  };
+}
